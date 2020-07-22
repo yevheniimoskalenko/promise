a33cf151626dcee285f17e267cfd7177d3df92e9
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-07-23 01:42:56 +0300

    finally

--- promise/promise.test.js
+++ promise/promise.test.js
@@ -1,7 +1,56 @@
-let yevheniiPromise
-describe('yevhenii promise', ()=>{
- test('should exists  and to by function', ()=>{
-    expect(yevheniiPromise).toBeDefined()
-    expect(typeOf yevheniiPromise).toBe('function')
- })
+const YevheniiPromise = require('./promise')
+
+const t = setTimeout
+
+describe('Vladilen Promise: ', () => {
+
+  let promise
+  let executorSpy
+
+  const successResult = 42
+  const errorResult = 'I am error'
+
+  beforeEach(() => {
+    executorSpy = jest.fn(r => t(() => r(successResult), 150))
+    promise = new YevheniiPromise(executorSpy)
+  })
+
+  test('should exists and to be typeof function', () => {
+    expect(YevheniiPromise).toBeDefined()
+    expect(typeof YevheniiPromise).toBe('function')
+  })
+
+  test('instance should have methods: then, catch, finally', () => {
+    expect(promise.then).toBeDefined()
+    expect(promise.catch).toBeDefined()
+    expect(promise.finally).not.toBeUndefined()
+  })
+
+  test('should call executor function', () => {
+    expect(executorSpy).toHaveBeenCalled()
+  })
+
+  test('should get data in then block and chain them', async () => {
+    const result = await promise.then(num => num).then(num => num * 2)
+    expect(result).toBe(successResult * 2)
+  })
+
+  test('should catch error', () => {
+    const errorExecutor = (_, r) => t(() => r(errorResult), 150)
+    const errorPromise = new VladilenPromise(errorExecutor)
+
+    return new Promise(resolve => {
+      errorPromise.catch(error => {
+        expect(error).toBe(errorResult)
+        resolve()
+      })
+    })
+  })
+
+  test('should call finally method', async () => {
+    const finallySpy = jest.fn(() => {})
+    await promise.finally(finallySpy)
+
+    expect(finallySpy).toHaveBeenCalled()
+  })
 })
