40f76e7e44e814caf1db898ae57e81fb76f4d84d
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-07-23 00:45:25 +0300

    init project and install jest -D and create class

---
+++ promise/promise.test.js
@@ -0,0 +1,7 @@
+let yevheniiPromise
+describe('yevhenii promise', ()=>{
+ test('should exists  and to by function', ()=>{
+    expect(yevheniiPromise).toBeDefined()
+    expect(typeOf yevheniiPromise).toBe('function')
+ })
+})
